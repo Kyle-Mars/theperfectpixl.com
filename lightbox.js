da1a2b62ef6488b3212c80c469b9ec67d0559e31
KyleMarshall1515 <retropixel15@gmail.com>
2023-01-16 09:19:56 -0600

    Removing unneeded JS

--- lightbox.js
+++ lightbox.js
@@ -6,10 +6,7 @@ function changeSlide(n) {showSlide(slideIndex += n);};
 function toSlide(n) {showSlide(slideIndex = n);};
 function showSlide(n) {
     const slides = document.getElementsByClassName('slide');
-    let modalPreviews = document.getElementsByClassName('modal-preview');
     if (n > slides.length) {slideIndex = 1;};
     if (n < 1) {slideIndex = slides.length;};
     for (let i = 0; i < slides.length; i++) {slides[i].style.display = "none";};
-    for (let i = 0; i < modalPreviews.length; i++) {modalPreviews[i].className = modalPreviews[i].className.replace(' active', '');};
-    slides[slideIndex - 1].style.display = 'block';
-    modalPreviews[slideIndex - 1].className += ' active';};
+    slides[slideIndex - 1].style.display = 'block';};
