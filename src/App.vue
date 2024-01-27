9d89a738e2d0a512fba7f21af73ebad1d5a29a8c
ThePerfectPixl <kylelmars@gmail.com>
2024-01-27 11:09:17 -0600

    Updated Build

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
 <script>
 import pageNav from "./components/Nav.vue"
 import pageFooter from "./components/Footer.vue"
-import data from "../public/data.js"
+import data from "./assets/data.js"
 
 export default {
   name: "App.vue",
