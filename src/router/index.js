7cb038d37d80bede389b41b3b4a7a87da27c9359
ThePerfectPixl <kylelmars@gmail.com>
2024-01-27 09:44:23 -0600

    Updating path

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.VITE_APP_BASE_PATH),
   routes: [
     {
       path: '/',
