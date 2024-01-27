eba1edccd5152df54876db019186d3162430622d
ThePerfectPixl <kylelmars@gmail.com>
2024-01-27 01:39:03 -0600

    Updated build

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueJsx(),
   ],
-  base: './',
+  base: '/theperfectpixl.com/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
