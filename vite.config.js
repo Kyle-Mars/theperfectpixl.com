1446a9b67ffa825499cd62039a02b2a08e31625a
ThePerfectPixl <kylelmars@gmail.com>
2024-01-26 19:40:11 -0600

    Preparing for launch

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueJsx(),
   ],
+  base: '/theperfectpixl.com/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
