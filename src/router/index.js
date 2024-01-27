2f1081f6ac78b4f55e86ee3b4dd2ce4299285453
ThePerfectPixl <kylelmars@gmail.com>
2024-01-27 11:49:21 -0600

    Returning to older version

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.VITE_APP_BASE_PATH),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
