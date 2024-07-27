067b7e9011c0217e550ea899160ee93e55b58e43
ThePerfectPixl <kylelmars@gmail.com>
2024-07-27 17:10:07 -0500

    Removing Router

--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@ import '../src/assets/styles/styles.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
 
 const app = createApp(App)
-
-app.use(router)
 app.mount('#app')
