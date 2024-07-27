3b3975aca87ce5f808d33c2d8debf6e7e88dfe2a
ThePerfectPixl <kylelmars@gmail.com>
2024-07-27 09:52:45 -0500

    Attempting router fix

--- src/router/index.js
+++ src/router/index.js
@@ -4,24 +4,21 @@ import Home from '../views/homePage.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    { path: '/:pathMatch(.*)*', redirect: '/' },
     {
       path: '/',
-      // name: 'Home',
       component: Home
     },
     {
       path: '/About',
-      // name: 'About',
       component: () => import('../views/aboutPage.vue')
     },
     {
       path: '/Gallery',
-      // name: 'Gallery',
       component: () => import('../views/galleryPage.vue')
     },
     {
       path: '/Showcase',
-      // name: 'Showcase',
       component: () => import('../views/showcasePage.vue')
     }
   ]
